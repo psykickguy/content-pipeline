00dbe8d6a77fd5b6f70f22e9b9000bc9d9f718bd
PsyKickGuy <shubhdas2005@gmail.com>
2025-12-31 16:27:01 +0530

    updated the the backend url

--- frontend/src/api/articles.js
+++ frontend/src/api/articles.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-const API_BASE = "http://content-pipeline.up.railway.app/articles";
+const API_BASE = "https://content-pipeline.up.railway.app/articles";
 
 export const fetchArticles = async () => {
   const res = await axios.get(API_BASE);
